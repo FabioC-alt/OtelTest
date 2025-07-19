d470cdeeda74bdd9b131d816d7b018a2f24e0c4c
Ubuntu <fabiociraci41@gmail.com>
2025-07-19 16:19:12 +0000

    instrumentation

--- backend/backend.py
+++ backend/backend.py
@@ -1,10 +1,26 @@
 from flask import Flask, jsonify
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+jaeger_exporter = JaegerExporter(
+    agent_host_name="jaeger",
+    agent_port=6831,
+)
+span_processor = BatchSpanProcessor(jaeger_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
 
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
 
 @app.route("/api/data")
 def data():
-    return jsonify(message="Hello from the backend!")
+    with tracer.start_as_current_span("handle /api/data"):
+        return jsonify(message="Hello from the backend!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
