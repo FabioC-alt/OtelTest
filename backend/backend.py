a62c4aaabf3e025bcedb1adb7b3823d8c1c05663
fabioc <fabiociraci41@gmail.com>
2025-07-20 13:04:13 +0200

    functioning version

--- backend/backend.py
+++ backend/backend.py
@@ -1,26 +1,12 @@
-from flask import Flask, jsonify
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace import TraceProvider
 
-trace.set_tracer_provider(TracerProvider())
-tracer = trace.get_tracer(__name__)
-jaeger_exporter = JaegerExporter(
-    agent_host_name="jaeger",
-    agent_port=6831,
-)
-span_processor = BatchSpanProcessor(jaeger_exporter)
-trace.get_tracer_provider().add_span_processor(span_processor)
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)
 
 @app.route("/api/data")
-def data():
-    with tracer.start_as_current_span("handle /api/data"):
-        return jsonify(message="Hello from the backend!")
+def get_data():
+    return jsonify(message="Hello from the backend!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,33 +1,16 @@
 from flask import Flask, jsonify
 import requests
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.jaeger.thrift import JaegerExporter
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-
-trace.set_tracer_provider(TracerProvider())
-tracer = trace.get_tracer(__name__)
-jaeger_exporter = JaegerExporter(
-    agent_host_name="jaeger",
-    agent_port=6831,
-)
-span_processor = BatchSpanProcessor(jaeger_exporter)
-trace.get_tracer_provider().add_span_processor(span_processor)
 
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
 
 @app.route("/api/forward")
-def forward():
-    with tracer.start_as_current_span("forward_request"):
-        try:
-            r = requests.get("http://backend:5000/api/data")
-            return jsonify(r.json())
-        except Exception as e:
-            return jsonify(error=str(e)), 500
+def forward_request():
+    try:
+        backend_response = requests.get("http://backend-service:5000/api/data")
+        backend_json = backend_response.json()
+        return jsonify(backend_json)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
